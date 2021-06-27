6679887fb462d46c7c10f13353d8860dabd8a009
Ashani Maleesha <ashani.g1.maleesha@gmail.com>
2021-06-27 16:18:07 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.querySelector('.img-btn').addEventListener('click',function()
+{
+    document.querySelector('.cont').classList.toggle('s-signup')
+}
+
+
+);
+/*
+<div><div id="body3">
+            <div class="foot">
+                <ul>
+                    <li><a href="#"><img src="/images/facebook.png" style=height:40px;width:40px></a></li>
+                    <li><a href="#"><img src="/images/instagram.png" style=height:40px;width:40px></a></li>
+                    <li><a href="#"><img src="/images/linkedin.png" style=height:40px;width:40px></a></li>
+                    <li><a href="#"><img src="/images/twitter.png" style=height:40px;width:40px></a></li>
+                </ul>
+            </div>
+            <h4 style= color:rgb(124,117,117);font-size:15px;padding:3px;text-align:center;margin-top:-30px;>
+                Copyright©2021 ABC National College</h4>
+        
+        </div></div>
+
+        <div><div id="body2">
+                <div>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                        <div style="align-items: center;">
+                            @include('admin.adding')
+                        </div>
+                    
+                    <br>
+                    <br>
+                    <br>
+                </div>
+        </div></div>
+
+        */
